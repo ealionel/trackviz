7445a35f1b6df2e013f0ae22df66f056ecfeaeb8
Tobide <helitobide@gmail.com>
2020-05-20 20:45:16 +0200

    Added resampling policy modularity Waveforms looks ok

--- src/index.ts
+++ src/index.ts
@@ -6,20 +6,37 @@ const context = new AudioContext()
 
 async function main() {
     const app = new PIXI.Application({
-        width: 1900,
+        width: 1000,
         height: 256,
         antialias: true,
         transparent: false,
-        resolution: 0.5
+        resolution: 1
     })
 
     const buffer = await loadAudioBuffer(context, './song.mp3')
 
     const frame = new Frame(app)
 
-    frame.draw(buffer)
+    frame.loadBuffer(buffer)
+    frame.draw()
 
     document.body.appendChild(app.view)
+
+    const offset = document.createElement('input')
+    offset.id = 'offset'
+    offset.type = 'range'
+    offset.min = '0'
+    offset.max = Math.ceil(buffer.length / frame.sampleWindow).toString()
+    offset.step = '50'
+
+    offset.oninput = ev => {
+        frame.offset = parseInt(offset.value)
+        frame.draw()
+    }
+
+    document.body.appendChild(offset)
+
+
 
 }
 
--- src/waveform.ts
+++ src/waveform.ts
@@ -1,30 +1,102 @@
 import * as PIXI from 'pixi.js'
 
+interface FrameOptions {
+    offset?: number,
+    sampleWindow?: number,
+    buffer?: AudioBuffer
+}
+
 export class Frame {
     app: PIXI.Application
 
-    constructor(app: PIXI.Application) {
+    offset: number
+    sampleWindow: number
+
+    buffer: AudioBuffer | null
+    overview: Float32Array | null
+
+    line: PIXI.Graphics
+
+    constructor(app: PIXI.Application, options: FrameOptions = {}) {
         this.app = app
+
+        this.offset = options.offset || 0
+        this.sampleWindow = options.sampleWindow || 440
+
+        this.buffer = null
+        this.overview = null
+
+        this.line = new PIXI.Graphics()
     }
 
-    draw(buffer: AudioBuffer) {
-        const line = new PIXI.Graphics()
+    get actualWidth(): number {
+        return this.app.renderer.width / this.app.renderer.resolution
+    }
 
-        const data = buffer.getChannelData(0)
+    get actualHeight(): number {
+        return this.app.renderer.height / this.app.renderer.resolution
+    }
 
-        line.lineStyle(1, 0xFFFFFF, 1)
-        line.x = 0
+    loadBuffer(buffer: AudioBuffer) {
+        this.buffer = buffer
+        
+        this.overview = peaks(this.buffer.getChannelData(0), this.sampleWindow)
+    }
 
-        this.app.stage.addChild(line)
+    draw() {
+        if (this.overview == null) {
+            return;
+        }
 
-        for (let i = 0; i < this.app.renderer.width * 2; i++) {
-            const y = this.app.renderer.height/(this.app.renderer.resolution*2) + data[i+64000] * 128
-            if (i == 0) {
-                line.moveTo(i, y)
-            } else {
-                line.lineTo(i, y)
-            }
+        this.line.clear()
+        this.line.lineStyle(1, 0xFFFFFF, 1)
+        this.app.stage.addChild(this.line)
+
+
+        this.line.x = 0
+
+
+        for (let i = 0; i < this.actualWidth; i++) {
+            const y = this.actualHeight * (this.overview[i + this.offset] + 1) / 2
+            const my = this.actualHeight * (-this.overview[i + this.offset] + 1) / 2
+
+            // line.moveTo(i + 0.5, this.actualHeight / 2)
+            this.line.moveTo(i + 0.5, my)
+            this.line.lineTo(i + 0.5, y)
         }
     }
-
 }
+
+const averager: Resampler = (data: Float32Array, windowSize: number): Float32Array => {
+    const newLength = Math.ceil(data.length / windowSize)
+    const arr = new Float32Array(newLength)
+    
+    for (let i = 0; i < newLength; i++) {
+        let average = 0
+        for (let j = 0; j < windowSize; j++) {
+            average += data[i * windowSize + j]
+        }
+        arr[i] = average / windowSize
+    }
+
+    return arr
+}
+
+const peaks: Resampler = (data: Float32Array, windowSize: number): Float32Array => {
+    const newLength = Math.ceil(data.length / windowSize)
+    const arr = new Float32Array(newLength)
+    
+    for (let i = 0; i < newLength; i++) {
+        let max = 0
+        for (let j = 0; j < windowSize; j++) {
+            if (Math.abs(data[i * windowSize + j]) > Math.abs(max)) {
+                max = data[i * windowSize + j]
+            }
+        }
+        arr[i] = max
+    }
+
+    return arr
+}
+
+type Resampler = (data: Float32Array, windowSize: number) => Float32Array
